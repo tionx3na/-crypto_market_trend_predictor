2a76239f651eab575ca416435a29e138b25d6da3
xena <1runx3na2@gmail.com>
2021-11-18 22:31:36 +0530

    Data transformations

--- bigdata/test.py
+++ bigdata/test.py
@@ -1,18 +1,25 @@
 import yfinance as yf
 import pandas as pd
+import stringcase
 
+ip = input("Enter the name of the coin: \n")
+camel_ip = stringcase.capitalcase(ip)
 ticker_list = pd.read_csv("tickers.csv")
 ticker_list_name = ticker_list['name']
 name = 1
 count = 0
 item_count = 0
+
 for name in ticker_list_name:
-    if name == "Bitcoin":
+    if name == camel_ip:
         item_count = count
-        print(name)
     count = count + 1
-print(item_count)
-print(ticker_list['symbol'][item_count])
+
+if item_count == 0:
+    print("Error!")
+else:
+    ticker = ticker_list['symbol'][item_count]
+    print(ticker)
 
 
 
